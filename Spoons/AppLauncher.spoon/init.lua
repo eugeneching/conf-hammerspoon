c893a439ae23f352fc5f530231779bfb9987a0a1
Eugene Ching <eugene@enegue.com>
2020-05-18 15:48:06 +0800

    Add binding for Digital Color Meter

--- Spoons/AppLauncher.spoon/init.lua
+++ Spoons/AppLauncher.spoon/init.lua
@@ -16,12 +16,12 @@ function obj:start()
     hs.hotkey.bind({"cmd"}, 'f3', function() self.onlyShow('signal') end)
     hs.hotkey.bind({"cmd"}, 'f4', function() self.onlyShow('numi') end)
     hs.hotkey.bind({"cmd"}, 'f5', function() self.onlyShow('path finder') end)
-    hs.hotkey.bind({"cmd"}, 'f6', function() self.onlyShow('gitkraken') end)
+    hs.hotkey.bind({"cmd"}, 'f6', function() self.onlyShow('Digital Color Meter') end)
     hs.hotkey.bind({"cmd"}, 'f7', function() self.onlyShow('iterm') end)
     hs.hotkey.bind({"cmd"}, 'f8', function() self.onlyShow('visual studio code') end)
     hs.hotkey.bind({"cmd"}, 'f9', function() self.onlyShow('google chrome') end)
     hs.hotkey.bind({"cmd", "shift"}, 'f9', function() self.onlyShow('google chrome canary') end)
-    hs.hotkey.bind({"cmd"}, 'f12', function() self.onlyShow('standard notes') end)
+    hs.hotkey.bind({"cmd"}, 'f12', function() self.onlyShow('notion') end)
 end
 
 return obj
