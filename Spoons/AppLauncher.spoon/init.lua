557c34846c2400fdc1134554d01a06b98f327ddd
Eugene Ching <eugene.ching@qavar.com>
2020-09-28 11:58:21 +0800

    Change to Inkdrop

--- Spoons/AppLauncher.spoon/init.lua
+++ Spoons/AppLauncher.spoon/init.lua
@@ -21,7 +21,7 @@ function obj:start()
     hs.hotkey.bind({"cmd"}, 'f8', function() self.onlyShow('visual studio code') end)
     hs.hotkey.bind({"cmd"}, 'f9', function() self.onlyShow('google chrome') end)
     hs.hotkey.bind({"cmd", "shift"}, 'f9', function() self.onlyShow('microsoft edge') end)
-    hs.hotkey.bind({"cmd"}, 'f12', function() self.onlyShow('nuclino') end)
+    hs.hotkey.bind({"cmd"}, 'f12', function() self.onlyShow('inkdrop') end)
 end
 
 return obj
