2244c4f5f1c8cd51e92a21080a323466e3403544
Eugene Ching <eugene.ching@qavar.com>
2022-09-16 15:36:06 +0800

    Update

--- Spoons/AppLauncher.spoon/init.lua
+++ Spoons/AppLauncher.spoon/init.lua
@@ -8,15 +8,14 @@ obj.author = "Eugene Ching"
 
 function obj:start()
     hs.hotkey.bind({"cmd"}, 'f1', function() hs.application.launchOrFocus('slack') end)
-    hs.hotkey.bind({"cmd"}, 'f3', function() hs.application.launchOrFocus('signal') end)
     hs.hotkey.bind({"cmd"}, 'f4', function() hs.application.launchOrFocus('numi') end)
     hs.hotkey.bind({"cmd"}, 'f5', function() hs.application.launchOrFocus('forklift') end)
-    hs.hotkey.bind({"cmd"}, 'f6', function() hs.application.launchOrFocus('Digital Color Meter') end)
     hs.hotkey.bind({"cmd"}, 'f7', function() hs.application.launchOrFocus('iterm') end)
     hs.hotkey.bind({"cmd"}, 'f8', function() hs.application.launchOrFocus('visual studio code') end)
     hs.hotkey.bind({"cmd"}, 'f9', function() hs.execute('open -n -a "Google Chrome" --args --profile-directory="Default" about:blank') end)
     hs.hotkey.bind({"cmd", "shift"}, 'f9', function() hs.application.launchOrFocus('google chrome') end)
     hs.hotkey.bind({"cmd"}, 'f12', function() hs.application.launchOrFocus('notion') end)
+    hs.hotkey.bind({}, 'f15', function() hs.application.launchOrFocus("todoist") end)
 end
 
 return obj
