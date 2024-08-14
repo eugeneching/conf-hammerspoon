adc142c40225ba20709b5a235ef60859c9086dcd
Eugene Ching <eugene.ching@qavar.com>
2024-08-14 10:56:47 +0800

    Update Fantastical binding

--- Spoons/AppLauncher.spoon/init.lua
+++ Spoons/AppLauncher.spoon/init.lua
@@ -8,17 +8,18 @@ obj.author = "Eugene Ching"
 
 function obj:start()
     hs.hotkey.bind({"cmd"}, 'f1', function() hs.application.launchOrFocus('slack') end)
-    hs.hotkey.bind({"cmd"}, 'f2', function() hs.application.launchOrFocus('whatsapp') end)
-    hs.hotkey.bind({"cmd"}, 'f3', function() hs.application.launchOrFocus('telegram') end)
+    hs.hotkey.bind({"cmd"}, 'f2', function() hs.application.launchOrFocus('fantastical') end)
+    hs.hotkey.bind({"cmd"}, 'f3', function() hs.application.launchOrFocus('whatsapp') end)
     hs.hotkey.bind({"cmd"}, 'f4', function() hs.application.launchOrFocus('numi') end)
     hs.hotkey.bind({"cmd"}, 'f5', function() hs.application.launchOrFocus('forklift') end)
-    hs.hotkey.bind({"cmd"}, 'f6', function() hs.application.launchOrFocus('standard notes') end)
+    hs.hotkey.bind({"cmd"}, 'f6', function() hs.application.launchOrFocus('telegram') end)
     hs.hotkey.bind({"cmd"}, 'f7', function() hs.application.launchOrFocus('iterm') end)
     hs.hotkey.bind({"cmd"}, 'f8', function() hs.application.launchOrFocus('visual studio code') end)
-    -- hs.hotkey.bind({"cmd"}, 'f9', function() hs.execute('open -n -a "Google Chrome" --args --profile-directory="Default" about:blank') end)
     hs.hotkey.bind({"cmd"}, 'f9', function() hs.application.launchOrFocus('google chrome') end)
     hs.hotkey.bind({"cmd"}, 'f12', function() hs.application.launchOrFocus('notion') end)
     hs.hotkey.bind({}, 'f15', function() hs.application.launchOrFocus("reminders") end)
+
+    -- hs.hotkey.bind({"cmd"}, 'f9', function() hs.execute('open -n -a "Google Chrome" --args --profile-directory="Default" about:blank') end)
 end
 
 return obj
